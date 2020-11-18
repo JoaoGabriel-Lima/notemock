28f06227e0f0f6f7b886ce9e0b855a06d3730a2d
João Lima <jg.limamarinho202@gmail.com>
2020-11-18 16:54:17 -0300

    Email Input

--- pages/components/maintenance_body/body.tsx
+++ pages/components/maintenance_body/body.tsx
@@ -8,6 +8,8 @@ export default class BodyMaintenance extends Component {
         <div className={styles.persistcontent}>
             <h1 className={styles.title}>Site ainda em <span className={styles.bold}>Desenvolvimento</span></h1>
             <p className={styles.description}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit.</p>
+
+            <input className={[styles.newsletter, styles.emailbox].join(' ')} placeholder="Seu endereço de email" type="email"></input>
             <button className={styles.newsletter}>
                 <a href="">Me avise quando estiver pronto</a>
             </button>
